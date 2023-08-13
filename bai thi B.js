2872b7bf3d134c69d2649c614a25bd3793490389
Mrlongtrang <gorkysandy02@gmail.com>
2023-08-13 18:41:41 +0700

    upload 2

---
+++ bai thi B.js
@@ -0,0 +1,15 @@
+const setBg = () => {
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    document.body.style.backgroundColor = "#" + randomColor;
+    color.innerHTML = "#" + randomColor;
+  }
+  
+  genNew.addEventListener("click", setBg);
+  setBg();
+  
+  function copyText(){
+    const text = document.getElementById('color').innerText
+    const btnText = document.getElementById('btn')
+    navigator.clipboard.writeText(text);
+    btnText.innerText = "Copied Text"
+  }
